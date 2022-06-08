359e6d19ac6f9111d7d7afc179deaca33d98ae19
SkyRafa <rafael.barrosparigi@sky.uk>
2022-06-08 11:33:23 +0100

    auth0 implemented

--- messages-api/server.js
+++ messages-api/server.js
@@ -1,22 +1,18 @@
 import express from "express";
 import cors from "cors";
 import indexRouter from "./routes/index.js";
-import basicAuth from "express-basic-auth";
-import { myAsyncAuthorizer, getUnauthorizedResponse } from "./services/passwordsServices.js";
+import { auth } from "express-oauth2-jwt-bearer";
 
 const app = express();
 app.use(cors());
 
-app.use(
-  basicAuth({
-    authorizer: myAsyncAuthorizer,
-    unauthorizedResponse: getUnauthorizedResponse,
-    authorizeAsync: true,
-    challenge: true,
-  })
-);
+// create middleware for checking the JWT
+const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+});
 
-app.use("/", indexRouter);
+app.use("/", checkJwt, indexRouter);
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`⚡️Server is running at https://localhost:${PORT}`);
